8958f5e303855fd881a700c99167e03bf0ee0573
DeifDj <clikball@gmail.com>
2024-02-02 17:57:07 -0500

    Implementación del formulario de inicio de sesión y reserva

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  templateUrl: './log-In.component.html',
+  styleUrls: ['./log-In.component.css']
 })
 export class LogInComponent {
-
+  loginForm!: NgForm;
+  ngOnInit() {
+    this.loginForm = {} as NgForm;
+  }
+onSubmit() {
+  console.log(this.loginForm.value);
+}
+  username!: String;
+  password!: String;
+  // Puedes añadir la lógica para iniciar sesión aquí
 }
